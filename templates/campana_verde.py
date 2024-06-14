2c5e92e7341d1effc011aeca345677377c32156e
Benzagon <gonbenzaquen@gmail.com>
2024-06-14 12:13:17 -0300

    feat: Clase CampanaVerde

--- templates/campana_verde.py
+++ templates/campana_verde.py
@@ -1,13 +1,25 @@
-class CampanaVerde:
-    def __init__(self, ...):
-        ''' completar docstring '''
-        pass
+from haversine import haversine, Unit
 
-    def distancia(self, ...) -> ...:
+class CampanaVerde:
+    def __init__(self, direccion:str, barrio:str, comuna:int, materiales:set[str], coord:tuple[float,float]):
         ''' completar docstring '''
-        pass
+        self.direccion:str = direccion
+        self.barrio:str = barrio
+        self.comuna:int = comuna
+        self.materiales:set[str] = materiales
+        self.latitud:float = coord[0]
+        self.longitud:float = coord[1] 
+
+    def distancia(self, lat:float, lng:float) -> float:
+        ''' Requiere: Nada
+            Devuelve: la distancia entre la CampanaVerde y el punto ingresado medida en metros 
+        '''
+        punto_campana:tuple[float ,float] = (self.latitud, self.longitud)
+        punto_ingresado:tuple[float ,float] = (lat, lng)
+        return haversine(punto_campana, punto_ingresado, unit=Unit.METERS)
 
     def __repr__(self) -> str:
-        ''' completar docstring '''
-        pass
-
+        ''' Requiere: Nada
+            Devuelve: La representación como string de la CampanaVerde en el siguiente formato: <dir@mater/iales@barrio>
+        '''
+        return "<" + self.direccion + "@" + "/".join(self.materiales) + "@" + self.barrio + ">"

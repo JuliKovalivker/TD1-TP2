4c6b122267b5ada701e0078afffc2de3a2f1a2b1
Benzagon <gonbenzaquen@gmail.com>
2024-06-21 17:46:38 -0300

    seguimos los tests de dataset

--- templates/dataset.py
+++ templates/dataset.py
@@ -15,7 +15,7 @@ def sortTresCampanas(xs:list[CampanaVerde], lat: float, lng: float) -> None:
     '''
     for i in range(len(xs)):
         if xs[i].distancia(lat, lng) < xs[i-1].distancia(lat, lng):
-            (xs[i], xs[i-1]) = (xs[i-1], xs[i]) 
+            (xs[i], xs[i-1]) = (xs[i-1], xs[i])
 
 def estaEnSet(set1: set[CampanaVerde], set2: set[CampanaVerde]) -> bool:
     '''Requiere: Nada.
@@ -25,8 +25,11 @@ def estaEnSet(set1: set[CampanaVerde], set2: set[CampanaVerde]) -> bool:
 
 class DataSetCampanasVerdes:
     def __init__(self, archivo_csv:str) -> None:
-        ''' Requiere: archivo_csv exista y sea del tipo .csv 
-            Devuelve: Nada
+        ''' Requiere: archivo_csv exista y sea del tipo .csv.
+                      Debe contar con las siguientes columnas: WKT;direccion;barrio;comuna;materiales
+                      Los datos en la columna WKT deben estar en el siguiente formato, donde "lat" y "lon" representan números decimales: "POINT (lat lon)".
+                      Los datos en la columna materiales deben estar en el siguiente formato, donde "mat1" y "mat2" representan texto plano: "mat1 / mat2"
+            Devuelve: Nada.
         '''
         self.campanas_verdes:list[CampanaVerde] = []
         self.cantidad:int = 0
@@ -45,13 +48,13 @@ class DataSetCampanasVerdes:
         f.close()
 
     def tamano(self) -> int:
-        ''' Requiere: Nada
+        ''' Requiere: Nada.
             Devuelve: La cantidad de campanas verdes en el dataset d.
         '''
         return self.cantidad
 
     def barrios(self) -> set[str]:
-        ''' Requiere: Nada
+        ''' Requiere: Nada.
             Devuelve: El conjunto de todos los barrios existentes en el dataset d.
         '''
         barrios:set[str] = set()
@@ -60,10 +63,9 @@ class DataSetCampanasVerdes:
         return barrios 
 
     def campanas_del_barrio(self, barrio:str) -> list[CampanaVerde]:
-        ''' Requiere: El nombre del barrio no tenga tilde
+        ''' Requiere: El nombre del barrio esté en el mismo formato que en el archivo .csv ingresado.
             Devuelve: Las instancias de CampanaVerde del dataset correspondientes al barrio indicado.
         '''
-        barrio = barrio.upper()
         campanas_barrio:list[CampanaVerde] = []
         for campana in self.campanas_verdes:
             if campana.barrio == barrio:
@@ -71,7 +73,7 @@ class DataSetCampanasVerdes:
         return campanas_barrio
 
     def cantidad_por_barrio(self, material:str) -> dict[str, int]:
-        ''' Requiere: La primera letra en mayúscula, y se usen tildes.
+        ''' Requiere: El nombre del material esté en el mismo formato que en el archivo .csv ingresado.
             Devuelve: La cantidad de campanas verdes en cada barrio en las que se puede depositar el material indicado.
         '''
         vr:dict[str,int] = {}
@@ -84,7 +86,7 @@ class DataSetCampanasVerdes:
         return vr
 
     def tres_campanas_cercanas(self, lat:float, lng:float) -> tuple[CampanaVerde, CampanaVerde, CampanaVerde]:
-        ''' Requiere: Nada.
+        ''' Requiere: self.tamano() >= 3.
             Devuelve: Las tres campanas verdes más cercanas al punto ingresado.
         '''
         tres_mas_cercanas:list[CampanaVerde] = [self.campanas_verdes[0], self.campanas_verdes[1], self.campanas_verdes[2]]
@@ -94,6 +96,7 @@ class DataSetCampanasVerdes:
                 tres_mas_cercanas.pop()
                 tres_mas_cercanas.append(campana)
                 sortTresCampanas(tres_mas_cercanas, lat, lng)
+        return tuple(tres_mas_cercanas)
 
     def exportar_por_materiales(self, archivo_csv:str, materiales:set[str]) -> None:
         '''Requiere: Nada.
--- templates/dataset_test.py
+++ templates/dataset_test.py
@@ -3,8 +3,10 @@ from dataset import DataSetCampanasVerdes
 from campana_verde import CampanaVerde
 
 ####################################################################
+
 # Tests para el método __init__()
 class TestInit(unittest.TestCase):
+
     # Crear un dataset con ninguna campana verde
     def test_ninguna_campana(self):
         dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/empty.csv")
@@ -27,6 +29,139 @@ class TestInit(unittest.TestCase):
         expected_campana = CampanaVerde("DIR","BAR",1,{"Papel"}, (1.0, 1.0))
         expected_output:list[CampanaVerde] = [expected_campana, expected_campana, expected_campana]
         self.assertEqual(output, expected_output)
+
 ####################################################################
+
+# Tests para el método tamano()
+class TestTamano(unittest.TestCase):
+
+    # Tamaño de un dataset con ninguna campana verde
+    def test_ninguna_campana(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/empty.csv")
+        output:int = dataset.tamano()
+        expected_output:int = 0
+        self.assertEqual(output, expected_output)
+
+    # Tamaño de un dataset con 1 campana verde
+    def test_una_campana(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/una_campana.csv")
+        output:int = dataset.tamano()
+        expected_output:int = 1
+        self.assertEqual(output, expected_output)
+        
+    # Tamaño de un dataset con varias campanas verde
+    def test_varias_campanas(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_iguales.csv")
+        output:int = dataset.tamano()
+        expected_output:int = 3
+        self.assertEqual(output, expected_output)
+
+####################################################################
+
+# Tests para el método barrios()
+class TestBarrios(unittest.TestCase):
+
+    # Un dataset vacío, sin barrios
+    def test_vacio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/empty.csv")
+        output:set[str] = dataset.barrios()
+        expected_output:set[str] = set()
+        self.assertSetEqual(output, expected_output)
+
+    # Varias campanas, todas con el mismo barrio
+    def test_mismo_barrio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_iguales.csv")
+        output:set[str] = dataset.barrios()
+        expected_output:set[str] = {"BAR"}
+        self.assertSetEqual(output, expected_output)
+
+    # Todas las campanas pertenecen a barrios distintos
+    def test_varios_barrios(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_distintas.csv")
+        output:set[str] = dataset.barrios()
+        expected_output: set[str] = {"CHACARITA", "BOEDO", "MONTE CASTRO"}
+        self.assertSetEqual(output, expected_output)
+
+####################################################################
+
+# Tests para el método campanas_del_barrio()
+class TestCampanasDelBarrio(unittest.TestCase):
+
+    # Ninguna CampanaVerde pertenece al barrio
+    def test_ninguna_en_barrio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_distintas.csv")
+        output:list[CampanaVerde] = dataset.campanas_del_barrio("TEST")
+        expected_output:list[CampanaVerde] = []
+        self.assertEqual(output, expected_output)
+
+    # Una sola CampanaVerde pertenece al barrio
+    def test_una_en_barrio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_distintas.csv")
+        output:list[CampanaVerde] = dataset.campanas_del_barrio("CHACARITA")
+        expected_output_len:int = 1
+        expected_output_barrio:str = "CHACARITA"
+        self.assertEqual(len(output), expected_output_len)
+        self.assertEqual(output[0].barrio, expected_output_barrio)
+
+    # Varias CampanaVerde pertenecen al barrio
+    def test_varias_en_barrio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_iguales.csv")
+        output:set[str] = dataset.campanas_del_barrio("BAR")
+        expected_output_len:int = 3
+        expected_output_barrio:str = "BAR"
+        self.assertEqual(len(output), expected_output_len)
+        for campana in output:
+            self.assertEqual(campana.barrio, expected_output_barrio)
+
+####################################################################
+
+# Tests para el método cantidad_por_barrio()
+class TestCantidadPorBarrio(unittest.TestCase):
+    
+    # El material no se puede depositar en ningún barrio
+    def test_ningun_barrio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_iguales.csv")
+        output:dict[str, int] = dataset.cantidad_por_barrio("Cartón")
+        expected_output:dict[str, int] = {}
+        self.assertDictEqual(output, expected_output)
+
+    # El material se puede depositar en 1 campana en un solo barrio
+    def test_una_campana_un_barrio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_distintas.csv")
+        output:dict[str, int] = dataset.cantidad_por_barrio("Cartón")
+        expected_output:dict[str, int] = {"CHACARITA": 1}
+        self.assertDictEqual(output, expected_output)
+
+    # El material se puede depositar en varias campanas en un solo barrio
+    def test_varias_campana_un_barrio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_iguales.csv")
+        output:dict[str, int] = dataset.cantidad_por_barrio("Papel")
+        expected_output:dict[str, int] = {"BAR": 3}
+        self.assertDictEqual(output, expected_output)
+
+    # El material se puede depositar en una campana en varios barrios
+    def test_una_campana_varios_barrio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_distintas.csv")
+        output:dict[str, int] = dataset.cantidad_por_barrio("Papel")
+        expected_output:dict[str, int] = {"CHACARITA": 1, "BOEDO": 1, "MONTE CASTRO": 1}
+        self.assertDictEqual(output, expected_output)
+
+    # El material se puede depositar en varias campanas en varios barrios
+    def test_varias_campana_varios_barrio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_mismos_materiales.csv")
+        output:dict[str, int] = dataset.cantidad_por_barrio("Papel")
+        expected_output:dict[str, int] = {"CHACARITA": 2, "BOEDO": 2}
+        self.assertDictEqual(output, expected_output)
+
+####################################################################
+
+# Tests para el método tres_campanas_cercanas()
+class TestTresCampanasCercanas(unittest.TestCase):
+    
+    # Las tres campanas más cercanas de un dataset vacío
+    def test_vacio(self):
+        dataset:DataSetCampanasVerdes = DataSetCampanasVerdes("./dataset_test_files/varias_campanas_iguales.csv")
+        output:tuple[CampanaVerde, CampanaVerde, CampanaVerde] = dataset.tres_campanas_cercanas(0.0,0.0)
+        print(output)
 
 unittest.main()
